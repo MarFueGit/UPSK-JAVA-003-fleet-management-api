bde9459171f37e2eed9f54fa56af8abc7e2e66da
MarFueGit <maricelafuentes100@gmail.com>
2024-02-12 19:52:54 -0600

    se agrego cambios en swuagger a

--- fleet-management-api/fleet-management-api/src/main/java/com/fleetmanagementapi/swagger/SwaggerConfig.java
+++ fleet-management-api/fleet-management-api/src/main/java/com/fleetmanagementapi/swagger/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.fleetmanagementapi.controllers")) // Reemplaza con el paquete de tus controladores
+                .apis(RequestHandlerSelectors.basePackage("com.fleetmanagementapi.controllers ")) // Reemplaza con el paquete de tus controladores
                 .paths(PathSelectors.any())
                 .build();
     }
